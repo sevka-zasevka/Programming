ae220c5af9c6b1e23566dd9af827b2b8c00d146a
sevka-zasevka <124648806+sevka-zasevka@users.noreply.github.com>
2024-03-15 17:03:31 +0700

    INotifyPropertyCheng....

---
+++ src/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    /// <summary>
+    /// Класс для хранения контактов.
+    /// </summary>
+    internal class Contact
+    {
+        /// <summary>
+        /// Имя контакта.
+        /// </summary>
+        private string _name;
+        /// <summary>
+        /// Телефон контакта.
+        /// </summary>
+        private string _phoneNumber;
+        /// <summary>
+        /// Электронная почта контакта.
+        /// </summary>
+        private string _email;
+
+        public string Name 
+        { 
+            get => _name; 
+            set => _name = value;
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => _phoneNumber = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
---
+++ src/Contacts/Contacts/ViewModel/MainVM.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using View.Model;
+
+namespace View.ViewModel
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        private Contact _contact;
+
+        public string Name
+        {
+            get
+            {
+                return _contact.Name;
+            }
+
+            set
+            {
+                if (_contact.Name != value)
+                {
+
+                    _contact.Name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _contact.PhoneNumber;
+            }
+
+            set
+            {
+                if (_contact.PhoneNumber != value)
+                {
+                    _contact.PhoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _contact.Email;
+            }
+
+            set
+            {
+                if (_contact.Email != value)
+                {
+                    _contact.Email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
+ 
